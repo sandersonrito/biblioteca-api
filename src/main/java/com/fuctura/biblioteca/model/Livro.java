63f9a400951fc3aca2714ae650ef003d1ca87cb6
sandersonrito <sanderson_rito_15@hotmail.com>
2023-11-11 12:30:07 -0300

    Criação das classes do pacote model

---
+++ src/main/java/com/fuctura/biblioteca/model/Livro.java
@@ -0,0 +1,60 @@
+package com.fuctura.biblioteca.model;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+    private Categoria categoria;
+
+    public Livro(int id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Livro() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nome_autor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nome_autor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
